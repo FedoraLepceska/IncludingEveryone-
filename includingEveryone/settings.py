c967f19cb9ca81df6630b6f5c1f95cef4d2ebc30
flepceska <flepceska@gmail.com>
2022-07-02 19:52:25 +0200

    smth2

--- includingEveryone/settings.py
+++ includingEveryone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5xsg*jp#2s@83w&!@)=jm5rkbhbp+2$qrepyi*ew(9^(*rhax8'
+# SECRET_KEY = 'django-insecure-5xsg*jp#2s@83w&!@)=jm5rkbhbp+2$qrepyi*ew(9^(*rhax8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
