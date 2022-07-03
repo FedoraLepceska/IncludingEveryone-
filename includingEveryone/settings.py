0aeec42d7a4b199eccbeb3c86cf6ce626eeb9906
flepceska <flepceska@gmail.com>
2022-07-03 18:09:00 +0200

    procfile

--- includingEveryone/settings.py
+++ includingEveryone/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.example.com', '.example.heroku.com', '.heroku.com']
+ALLOWED_HOSTS = ['https://including-everyone.herokuapp.com/']
 
 
 # Application definition
