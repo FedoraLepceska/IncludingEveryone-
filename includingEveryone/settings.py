4333ba0fdbcfcec123dd03cc2ff6765a3eb4c355
flepceska <flepceska@gmail.com>
2022-07-03 18:04:30 +0200

    procfile

--- includingEveryone/settings.py
+++ includingEveryone/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.example.com', '.example.heroku.com', '.heroku.com']
 
 
 # Application definition
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'app.apps.AppConfig',
     'embed_video',
     'fontawesomefree',
-    'bootstrap5',
     'django_heroku'
 ]
 
